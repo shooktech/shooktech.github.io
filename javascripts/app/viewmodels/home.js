91cf8391c5df95253b8ea74d6886dfd0717d7fba
chimalsky <chimalsky@bitbucket.org>
2015-08-07 11:59:12 -0400

    spa dist

---
+++ javascripts/app/viewmodels/home.js
@@ -0,0 +1,30 @@
+﻿define(function(require) {
+	var app = require('durandal/app'),
+		ko = require('knockout'),
+		router = require('plugins/router');
+
+	var VmBase = require('./vm_base');
+
+    var VmHome = VmBase.extend({
+    	init: function() {
+    		this.carouselItems = [
+    			{
+    			img: "http://johnrichardthompson.com/images/photographs/mackinac.jpg",
+    			caption: "Caption city"
+    			},
+    			{
+    			img: "http://johnrichardthompson.com/images/photographs/camcorder.jpg",
+    			caption: "Fuck City"
+    			}
+    		];
+    	},
+
+    	activate: function() {
+    	},
+
+        
+    });
+
+  
+    return VmHome.create();
+});
