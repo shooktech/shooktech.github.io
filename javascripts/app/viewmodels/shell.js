2310bcdb5e7dab24f127d6d18b3028164e492aff
chimalsky <chimalsky@bitbucket.org>
2015-08-08 13:25:00 -0400

    Using Fastclick to get rid of mobile click delay

---
+++ javascripts/app/viewmodels/shell.js
@@ -0,0 +1,29 @@
+﻿define(function(require) {
+    var router = require('plugins/router');
+    var app = require('durandal/app');
+
+    var VmBase = require('./vm_base');
+
+    var VmShell = VmBase.extend({
+        router: router,
+    
+        activate: function () {
+            router.map([
+                // Non Nav
+                { route: '', title:'Home', moduleId: 'viewmodels/home', nav: false },
+
+                // Nav
+                { route: 'About', moduleId: 'viewmodels/about', nav: true },
+                { route: 'Careers', moduleId: 'viewmodels/careers', nav:true },
+            ]).buildNavigationModel();
+            
+            return router.activate();
+        },
+
+        compositionComplete: function() {
+
+        }
+    });
+
+    return VmShell;
+});
