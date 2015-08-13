2bdd27a38b95580e7f1657b32bcea3d5047b9613
chimalsky <chimalsky@bitbucket.org>
2015-08-13 17:47:58 -0400

    Detroit Shovels Harder

--- spa/assets/javascripts/app/viewmodels/home.js
+++ spa/assets/javascripts/app/viewmodels/home.js
@@ -28,7 +28,6 @@ define(function(require) {
 
         },
         bindingComplete: function() {
-            /*
             var carouselInner = $('.carousel-inner');
 
             carouselInner.swipe( {
@@ -39,7 +38,7 @@ define(function(require) {
                     $(this).parent().carousel('next');
                 },
                 threshold: 75
-            }); */
+            });
             
         },
         attached: function() {
